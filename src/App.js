f7a9a5ab9c777ff4f601f5a619579f0be179ffcd
hmacqueen19 <104796751+hmacqueen19@users.noreply.github.com>
2022-12-01 18:29:19 -0500

    Made some structural changes to my src pages and added a default city.

--- src/App.js
+++ src/App.js
@@ -1,142 +1,34 @@
-import React, { useState } from "react";
-import "./styles.css";
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
-import "./BlanketInformation.css";
-import BlanketInformation from "./BlanketInformation";
-import axios from "axios";
 
 export default function App() {
-  const [weatherData, setWeatherData] = useState({ ready: false });
-  function handleResponse(response) {
-    setWeatherData({
-      ready: true,
-      temperature: response.data.main.temp,
-      description: response.data.weather[0].description,
-      wind: response.data.wind.speed,
-      city: response.data.name,
-      humidity: response.data.main.humidity,
-      precipitation: 15,
-      icon: "",
-      iconDescription: "",
-    });
-  }
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
 
-  if (weatherData.ready) {
-    return (
-      <div className="App">
-        <div className="container">
-          <h1>The Crocheter's Almanac</h1>
-          <div className="weather-app-wrapper">
-            <div className="row">
-              <form id="search-form">
-                <div className="row">
-                  <div className="col-6">
-                    <input
-                      id="location-bar"
-                      className="location-bar"
-                      type="text"
-                      placeholder="Enter your location"
-                    />
-                  </div>
-                  <div className="col-3">
-                    <input
-                      id="search-button"
-                      className="search-button shadow"
-                      type="submit"
-                      value="Search"
-                    />
-                  </div>
-                  <div className="col-3">
-                    <input
-                      id="submit-button"
-                      className="current-location shadow"
-                      type="submit"
-                      value="Current Location"
-                    />
-                  </div>
-                </div>
-              </form>
-              <div className="col-12">
-                <div className="card current-weather">
-                  <div className="overview">
-                    <h2 id="current-location" className="card-title"></h2>
-                    <ul>
-                      <li id="last-updated">
-                        Last updated:{" "}
-                        <span id="current-date">{weatherData.date}</span>
-                      </li>
-                      <li id="description">{weatherData.description}</li>
-                    </ul>
-                  </div>
-                  <div className="row">
-                    <div className="col-6">
-                      <div className="clearfix weather-temperature">
-                        <img
-                          src={weatherData.icon}
-                          alt={weatherData.iconDescription}
-                          id="icon"
-                        />
-                        <div id="temperature-information">
-                          <strong id="temperature-current">
-                            {weatherData.temperature}
-                          </strong>
-                          <span className="units">
-                            <a href="#bar" id="fahrenheit" className="active">
-                              {" "}
-                              °F{" "}
-                            </a>{" "}
-                            |
-                            <a href="#foo" id="celsius">
-                              °C
-                            </a>
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-6" id="humidity-wind">
-                      <ul>
-                        <li>
-                          Precipitation:{" "}
-                          <span id="precipitation">
-                            {weatherData.precipitation}%
-                          </span>
-                        </li>
-                        <li>
-                          Humidity:{" "}
-                          <span id="humidity">{weatherData.humidity}%</span>
-                        </li>
-                        <li>
-                          Wind: <span id="wind">{weatherData.wind}km/h</span>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-                <div className="weather-forecast" id="forecast"></div>
-                <div className="col-12">
-                  <BlanketInformation />
-                </div>
-              </div>
-            </div>
-            <p>
-              <a
-                href="https://github.com/hmacqueen19/my-app-weather-react"
-                alt="GitHub link"
-              >
-                Open-source code
-              </a>{" "}
-              by Helen Macqueen
-            </p>
-          </div>
-        </div>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.delac.io/" target="_blank">
+            Helen Macqueen
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/hmacqueen19/my-app-weather-react"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://astounding-lebkuchen-2be8bb.netlify.app"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
-    );
-  } else {
-    const apiKey = "6235b58538eef339ac44773d06f22df9";
-    let city = "London";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
-    return "loading";
-  }
+    </div>
+  );
 }
---
+++ src/Weather.js
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import "./styles.css";
+import "./Weather.css";
+import "./BlanketInformation.css";
+import BlanketInformation from "./BlanketInformation";
+import axios from "axios";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      precipitation: 15,
+      icon: "",
+      iconDescription: "",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="App">
+        <div className="container">
+          <h1>The Crocheter's Almanac</h1>
+          <div className="weather-app-wrapper">
+            <div className="row">
+              <form id="search-form">
+                <div className="row">
+                  <div className="col-6">
+                    <input
+                      id="location-bar"
+                      className="location-bar"
+                      type="text"
+                      placeholder="Enter your location"
+                    />
+                  </div>
+                  <div className="col-3">
+                    <input
+                      id="search-button"
+                      className="search-button shadow"
+                      type="submit"
+                      value="Search"
+                    />
+                  </div>
+                  <div className="col-3">
+                    <input
+                      id="submit-button"
+                      className="current-location shadow"
+                      type="submit"
+                      value="Current Location"
+                    />
+                  </div>
+                </div>
+              </form>
+              <div className="col-12">
+                <div className="card current-weather">
+                  <div className="overview">
+                    <h2 id="current-location" className="card-title"></h2>
+                    <ul>
+                      <li id="last-updated">
+                        Last updated:{" "}
+                        <span id="current-date">{weatherData.date}</span>
+                      </li>
+                      <li id="description">{weatherData.description}</li>
+                    </ul>
+                  </div>
+                  <div className="row">
+                    <div className="col-6">
+                      <div className="clearfix weather-temperature">
+                        <img
+                          src={weatherData.icon}
+                          alt={weatherData.iconDescription}
+                          id="icon"
+                        />
+                        <div id="temperature-information">
+                          <strong id="temperature-current">
+                            {weatherData.temperature}
+                          </strong>
+                          <span className="units">
+                            <a href="#bar" id="fahrenheit" className="active">
+                              {" "}
+                              °F{" "}
+                            </a>{" "}
+                            |
+                            <a href="#foo" id="celsius">
+                              °C
+                            </a>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-6" id="humidity-wind">
+                      <ul>
+                        <li>
+                          Precipitation:{" "}
+                          <span id="precipitation">
+                            {weatherData.precipitation}%
+                          </span>
+                        </li>
+                        <li>
+                          Humidity:{" "}
+                          <span id="humidity">{weatherData.humidity}%</span>
+                        </li>
+                        <li>
+                          Wind: <span id="wind">{weatherData.wind}km/h</span>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className="weather-forecast" id="forecast"></div>
+                <div className="col-12">
+                  <BlanketInformation />
+                </div>
+              </div>
+            </div>
+            <p>
+              <a
+                href="https://github.com/hmacqueen19/my-app-weather-react"
+                alt="GitHub link"
+              >
+                Open-source code
+              </a>{" "}
+              by Helen Macqueen
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    const apiKey = "6235b58538eef339ac44773d06f22df9";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "loading";
+  }
+}
