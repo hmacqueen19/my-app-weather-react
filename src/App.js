46102a65e9fa59607a3e1f030486ff02e7e5cf20
hmacqueen19 <104796751+hmacqueen19@users.noreply.github.com>
2022-12-01 17:12:01 -0500

    Brought elements of weather app back into same component. Added precipitation to application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./styles.css";
 import "./App.css";
-import "./Form.css";
-import "./CurrentWeather.css";
 import "./BlanketInformation.css";
 import BlanketInformation from "./BlanketInformation";
-import CurrentWeather from "./CurrentWeather.js";
-import Form from "./Form.js";
+
 //import axios from "axios";
 
 export default function App() {
@@ -14,10 +11,80 @@ export default function App() {
       <div className="container">
         <h1>The Crocheter's Almanac</h1>
         <div className="weather-app-wrapper">
-          <Form />
           <div className="row">
+            <form id="search-form">
+              <div className="row">
+                <div className="col-6">
+                  <input
+                    id="location-bar"
+                    className="location-bar"
+                    type="text"
+                    placeholder="Enter your location"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    id="search-button"
+                    className="search-button shadow"
+                    type="submit"
+                    value="Search"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    id="submit-button"
+                    className="current-location shadow"
+                    type="submit"
+                    value="Current Location"
+                  />
+                </div>
+              </div>
+            </form>
             <div className="col-12">
-              <CurrentWeather />
+              <div className="card current-weather">
+                <div className="overview">
+                  <h2 id="current-location" className="card-title"></h2>
+                  <ul>
+                    <li id="last-updated">
+                      Last updated: <span id="current-date"></span>
+                    </li>
+                    <li id="description"></li>
+                  </ul>
+                </div>
+                <div className="row">
+                  <div className="col-6">
+                    <div className="clearfix weather-temperature">
+                      <img src="" alt="clear" id="icon" />
+                      <div id="temperature-information">
+                        <strong id="temperature-current"></strong>
+                        <span className="units">
+                          <a href="#bar" id="fahrenheit" className="active">
+                            {" "}
+                            °F{" "}
+                          </a>{" "}
+                          |
+                          <a href="#foo" id="celsius">
+                            °C
+                          </a>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-6" id="humidity-wind">
+                    <ul>
+                      <li>
+                        Precipitation: <span id="precipitation">%</span>
+                      </li>
+                      <li>
+                        Humidity: <span id="humidity">%</span>
+                      </li>
+                      <li>
+                        Wind: <span id="wind"></span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
               <div className="weather-forecast" id="forecast"></div>
               <div className="col-12">
                 <BlanketInformation />
